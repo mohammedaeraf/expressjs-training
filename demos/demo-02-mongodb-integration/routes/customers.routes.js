c37c5ad3ddc11c090abbdb8af91671f182ff1d95
Mohammed Aeraf <aeraf@outlook.com>
2026-02-01 19:31:44 +0530

    CORS

--- demos/demo-02-mongodb-integration/routes/customers.routes.js
+++ demos/demo-02-mongodb-integration/routes/customers.routes.js
@@ -12,6 +12,8 @@
  */
 
 const express = require("express");
+const cors = require("cors");
+
 const router = express.Router();
 const Customer = require("../models/Customer");
 const validateCustomer = require("../middlewares/validateCustomer");
--- demos/demo-02-mongodb-integration/server.js
+++ demos/demo-02-mongodb-integration/server.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://appifybrands.com"],
+  }),
+); // Enable CORS for all routes
 
 mongoose
   .connect(process.env.MONGO_URL)
